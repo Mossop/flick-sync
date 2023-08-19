5d10a206b66044ea63603dbc01dc58e97f02481f
Dave Townsend <dtownsend@oxymoronical.com>
2023-08-19 17:53:00 +0100

    Allow building the development client

---
+++ packages/mobile/app.config.js
@@ -0,0 +1,38 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+const LOGO = IS_DEV ? "./assets/logo-dev" : "./assets/logo";
+
+export default {
+  name: IS_DEV ? "Synced Flicks (Dev)" : "Synced Flicks",
+  slug: "flicksync",
+  version: "1.5.0",
+  orientation: "default",
+  icon: `${LOGO}/icon.png`,
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: `${LOGO}/splash.png`,
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: `${LOGO}/adaptive-icon.png`,
+      backgroundColor: "#ffffff",
+    },
+    blockedPermissions: ["android.permission.RECORD_AUDIO"],
+    package: IS_DEV
+      ? "com.oxymoronical.flicksync_dev"
+      : "com.oxymoronical.flicksync",
+  },
+  web: {
+    favicon: `${LOGO}/favicon.png`,
+  },
+  extra: {
+    eas: {
+      projectId: "94a5b9ed-95e6-4aed-b4cb-a04f40174d21",
+    },
+  },
+};
